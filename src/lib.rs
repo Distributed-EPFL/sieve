453d985abafb33cd7407ac7ffef906ca4fe5944b
Ogier Bouvier <ogier@bouvier.family>
2021-04-15 13:52:23 +0200

    remove unbatched version of sieve
    Signed-off-by: Ogier Bouvier <ogier@bouvier.family>

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,7 @@
 #![deny(missing_docs)]
 
-//! This crate provides two implementation of the `Sieve` consistent broadcast algorithm
+//! This crate provides an implementation of the `BatchedSieve` consistent broadcast algorithm. <br />
+//! See the examples directory for examples on how to use this in your application
 
-/// This module provides an implementation of basic `Sieve` supporting broadcast of single message.
-pub mod classic;
-
-/// This module provides an optimized version of `Sieve` using batching and selective acknowledgements, on
-/// top of batched `Murmur`.
-pub mod batched;
+mod batched;
+pub use batched::*;
